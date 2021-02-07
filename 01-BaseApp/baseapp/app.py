3e0e132a28558617eccbaaff7ad676acc958d088
Akbarsait <akbarsaitn.cfmx@gmail.com>
2021-02-06 23:08:33 -0500

    Adding the base repo

---
+++ 01-BaseApp/baseapp/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+def create_app():
+    """
+    Create a Flask application using the app pattern.
+    """
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        """
+        Render a Hello World response.
+        :return: Flask response
+        """
+        return 'Welcome to PyFlaskDokr Series!'
+
+    return app
